e83e2d6632a8e76ecc2ac9529b412f4bf1ec6327
Anderson-Batista <andersonbfs15@gmail.com>
2023-05-13 11:26:26 -0300

    Criacao tela de login

---
+++ app/src/main/java/br/edu/ifpe/recife/tads/pdm/taskinvest2/activity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifpe.recife.tads.pdm.taskinvest2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifpe.recife.tads.pdm.taskinvest2.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/br/edu/ifpe/recife/tads/pdm/taskinvest2/activity/LoginActivity.java
+++ app/src/main/java/br/edu/ifpe/recife/tads/pdm/taskinvest2/activity/LoginActivity.java
@@ -2,15 +2,22 @@ package br.edu.ifpe.recife.tads.pdm.taskinvest2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import br.edu.ifpe.recife.tads.pdm.taskinvest2.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void cadastrar(View view) {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
