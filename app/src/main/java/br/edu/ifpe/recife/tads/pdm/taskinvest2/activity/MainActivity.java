25f08636e3e91dd70793afa54a79abac6eafbcea
Anderson-Batista <andersonbfs15@gmail.com>
2023-05-13 10:18:22 -0300

    Criacao tela de cadastro

--- app/src/main/java/br/edu/ifpe/recife/tads/pdm/taskinvest2/activity/MainActivity.java
+++ app/src/main/java/br/edu/ifpe/recife/tads/pdm/taskinvest2/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package br.edu.ifpe.recife.tads.pdm.taskinvest2;
+package br.edu.ifpe.recife.tads.pdm.taskinvest2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import br.edu.ifpe.recife.tads.pdm.taskinvest2.R;
 
 public class MainActivity extends AppCompatActivity {
 
